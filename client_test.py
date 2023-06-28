e7a33ab7053263f03831bc53568c012a59eb183e
Valterius <dhshafer.73@gmail.com>
2023-06-27 19:41:31 -0500

    Finished task one

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
 from client3 import getDataPoint
+from client3 import getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -9,17 +10,49 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    actual_outcome = getDataPoint(quotes[0])[3]
+    expected_outcome = float(121.2 + 120.48) / 2
+
+    self.assertEqual(actual_outcome, expected_outcome)
+
+
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    bid_price = getDataPoint(quotes[0])[1]
+    ask_price = getDataPoint(quotes[0])[2]
+    self.assertGreater(bid_price, ask_price)
 
 
   """ ------------ Add more unit tests ------------ """
 
+  def test_getRatio_divideByZeroDoesNotThrowZeroDivisorError(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+       'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+       'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    price_a = getDataPoint(quotes[0])[3]
+    price_b = 0
 
+    self.assertEqual(getRatio(price_a, price_b), 0)
+
+  def test_getRatio_returnsExpectedValue(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+       'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+       'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    price_a = getDataPoint(quotes[0])[3]
+    price_b = getDataPoint(quotes[1])[3]
+    actual_value = getRatio(price_a, price_b)
+    expected_value = float(1.0005426841995408)
+    self.assertEqual(actual_value, expected_value)
 
 if __name__ == '__main__':
     unittest.main()
